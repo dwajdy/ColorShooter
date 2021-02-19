ac6fb94319df2b6fbbb9b150c076ac214ca26880
Wajdy D <wajde.dakwar@gmail.com>
2021-02-19 14:02:00 +0200

    Added Laser + Other Graphics Style Improvements
    Laster not accurate yet. Need to be fixed.

--- Assets/Scripts/GunMovement.cs
+++ Assets/Scripts/GunMovement.cs
@@ -15,7 +15,7 @@ public class GunMovement : MonoBehaviour
     {
         Vector3 vetor3 = new Vector3(Input.mousePosition.x,Input.mousePosition.y,10);
         Vector3 aimPos = Camera.main.ScreenToWorldPoint(vetor3);
-        aimPos.z = 10;
+        aimPos.z = this.transform.position.z + 10;
         this.transform.LookAt(aimPos);
         Debug.Log($"Aim position is: ({aimPos.x}, {aimPos.y}, {aimPos.z}).");
     }
