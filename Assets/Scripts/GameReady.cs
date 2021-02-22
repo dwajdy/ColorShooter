957bba12386587f9dacb34ae90dc5b0a0437f153
Wajdy D <wajde.dakwar@gmail.com>
2021-02-22 17:25:31 +0200

    Added wait text at start of round until game is ready

---
+++ Assets/Scripts/GameReady.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameReady : MonoBehaviour
+{
+    public GameObject gameManager;
+    private GameDynamics gameDynamics;
+    private float secondPassed = 0.0f;
+
+    private float secondsToUpdateText = 1.5f;
+    private bool previousLastGameIsReady = true;
+    private Text textComp;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameDynamics = gameManager.GetComponent<Settings>().GetGameDynamics();
+        textComp = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(! gameDynamics.IsGameReady() && (gameDynamics.IsGameStartedFirstTime() || previousLastGameIsReady == true))
+        {
+            textComp.enabled = true;
+            previousLastGameIsReady = gameDynamics.IsGameReady();
+            secondPassed += Time.deltaTime;
+            if(secondPassed > secondsToUpdateText)
+            {   
+                secondPassed = 0.0f;
+                if(textComp.text.Length < 16)
+                {
+                    textComp.text += ".";
+                }
+                else
+                {
+                    textComp.text = "WAIT..";
+                }
+
+                return;
+            }
+        }
+        else if(gameDynamics.IsGameReady())
+        {
+            textComp.enabled = false;
+            textComp.text = "WAIT..";
+            previousLastGameIsReady = true;
+        }
+    }
+}
